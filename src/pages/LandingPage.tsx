bea7b14fa9b4e5c656955d0a60ebdd960cc56520
Vijitta Alexander <vijittaalexander2101@gmail.com>
2025-10-28 16:02:01 +0530

    header integration (#5)
    * reusable components changes
    
    * landingpage changes
    
    * logo changes
    
    * style changes
    
    * component integration

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,41 +1,32 @@
 import deloitteLogo from "../icons/deloitte_logo.svg";
 import infoIcon from "../icons/Shape.png";
-import LandingPageImage from "../icons/landing_page_image.png";
+import LandingPageImage from "../icons/landing_page.svg";
 import "../LandingPage.css";
+import Header from "../layout/Header";
 function LandingPage() {
   return (
     <div className="container-main">
-      <div className="header">
-        <div className="left">
-          <div className="header-logo">
-            <img src={deloitteLogo} alt="deloitte logo" />
-          </div>
-          <div className="header-title">Beacon Digital Tagging</div>
-        </div>
-        <div className="header-info">
-          <img src={infoIcon} alt="info icon" />
-        </div>
-      </div>
+      <Header showLocationDropdown={false} showUserSection={false} />
       <div className="landing-main">
         <div className="left-section">
-          <div className="title">
-            Welcome to the Beacon <br />
-            Digital Tagging
-          </div>
-          <div className="content">
-            A secure internal platform for managing user access and 
-            engagement permissions in compliance with Deloitte policies.
-          </div>
-          <div className="login-button">
-            <button type="button" className="button">
-              Login
-            </button>
+            <div className="title">
+              Welcome to the Beacon <br />
+              Digital Tagging
+            </div>
+            <div className="content">
+              A secure internal platform for managing user access and engagement
+              permissions in compliance with Deloitte policies.
+            </div>
+            <div className="login-button">
+              <button type="button" className="button">
+                Login
+              </button>
           </div>
         </div>
         <div className="right-section">
-            <div className="image-container">
-              <img src={LandingPageImage} alt="Landing Page" />
-            </div>
+          <div className="image-container">
+            <img src={LandingPageImage} alt="Landing Page" />
+          </div>
         </div>
       </div>
     </div>
